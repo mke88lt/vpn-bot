703ac0a8b2836962d4a382a327341cb5eda3a1e5
mke88lt <mke88lt@gmail.com>
2026-06-05 02:22:58 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ PLANS = {
     "1month_10gb": {"name": "۱ ماهه - ۱۰ گیگ", "price": 250000, "days": 30, "volume_gb": 10},
     "1month_20gb": {"name": "۱ ماهه - ۲۰ گیگ", "price": 400000, "days": 30, "volume_gb": 20},
     "1month_30gb": {"name": "۱ ماهه - ۳۰ گیگ", "price": 550000, "days": 30, "volume_gb": 30},
-    "1month_60gb": {"name": "۱ ماهه - ۶۰ گیگ", "price": 1,250,000, "days": 30, "volume_gb": 60},
-    "1month_100gb": {"name": "۱ ماهه - ۱۰۰ گیگ", "price": 2,050,000, "days": 30, "volume_gb": 100},  
+    "1month_60gb": {"name": "۱ ماهه - ۶۰ گیگ", "price": 1250, "days": 30, "volume_gb": 60},
+    "1month_100gb": {"name": "۱ ماهه - ۱۰۰ گیگ", "price": 2050, "days": 30, "volume_gb": 100},  
 }
 
 CARD_NUMBER = "6219-8618-0978-1759"
